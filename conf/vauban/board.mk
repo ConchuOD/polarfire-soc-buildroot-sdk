c5ca57693914caab482aad4e47d4bdbb53e392fa
Conor Dooley <conor.dooley@microchip.com>
2024-04-18 08:24:31 +0100

    apr 2024 work
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/vauban/board.mk
+++ conf/vauban/board.mk
@@ -4,5 +4,6 @@ linux_dtb := $(riscv_dtbdir)/microchip/mpfs-vauban.dtb
 buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config_us_ub
 its_file := $(confdir)/32-bit-fit-image.its
 tftp_boot_scr ?= $(DEVKIT)-boot.scr
-UBOOT_DEFCONFIG := "microchip_mpfs_vauban"
+UBOOT_DEFCONFIG := "microchip_mpfs_vauban_work"
+HSS_SUPPORT ?= y
 export UBOOT_DEFCONFIG
