ce0c459b356aae2478e1725604ce881b3b8889da
Conor Dooley <mail@conchuod.ie>
2024-05-22 09:42:06 +0100

    conf: other misc updates
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- conf/vauban/board.mk
+++ conf/vauban/board.mk
@@ -1,6 +1,8 @@
 UBOOT_VERSION = 2022.01
+HSS_SUPPORT ?= y
 linux_defconfig := $(confdir)/defconfig
-linux_dtb := $(riscv_dtbdir)/microchip/mpfs-vauban.dtb
+linux_dtb := $(riscv_dtbdir)/microchip/mpfs-beaglev-fire.dtb
+# linux_dtb := $(riscv_dtbdir)/microchip/mpfs-vauban.dtb
 buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config_us_ub
 its_file := $(confdir)/32-bit-fit-image.its
 tftp_boot_scr ?= $(DEVKIT)-boot.scr
