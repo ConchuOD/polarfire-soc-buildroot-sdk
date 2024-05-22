62f1ef80cbb62af84fc3fc9d11f095b1d2378a65
Conor Dooley <mail@conchuod.ie>
2024-05-22 09:41:41 +0100

    k230/canmv configs
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

---
+++ conf/canmv/board.mk
@@ -0,0 +1,6 @@
+linux_defconfig := $(confdir)/$(DEVKIT)/defconfig
+linux_dtb := $(riscv_dtbdir)/canaan/k230-canmv.dtb
+buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config_no_ub
+its_file := $(confdir)/$(DEVKIT)/fit-image.its
+tftp_boot_scr := canmv-boot.scr
+
